843c33a3a0ce99916a338b2a2e79fd0a22b7b076
Bernd Beuster <bernd.beuster@creativechips.com>
2016-06-01 17:14:34 +0200

    Added audio.

--- rtl/radio_core.sv
+++ rtl/radio_core.sv
@@ -1,21 +1,25 @@
 /* Radio core */
 
 module radio_core
-  #(parameter width_dds    = 32,                          // DDS accumulator width
-    parameter width_cordic = 17,                          // CORDIC width
-    parameter M            = 240)                         // carrier to broad-band frequency ratio
-   (input  wire                             reset,        // reset
-    input  wire                             clk_s,        // sampling clock
-    input  wire                             clk_b,        // base-band clock
-    input  wire                             adc,          // broadcast signal from 1-bit ADC
-    input  wire        [width_dds - 1:0]    K,            // phase constant for DDS
-    output wire signed [width_cordic - 1:0] demodulated); // demodulated signal
+  #(parameter width_dds    = 32,                            // DDS accumulator width
+    parameter width_cordic = 17,                            // CORDIC width
+    parameter M1           = 250,                           // carrier to broad-band frequency ratio
+    parameter M2           = 30)                            // broad-band to audio frequency ratio
+   (input  wire                             reset,          // reset
+    input  wire                             clk_s,          // sampling clock
+    input  wire                             clk_b,          // base-band clock
+    input  wire                             clk_a,          // audio clock
+    input  wire                             adc,            // broadcast signal from 1-bit ADC
+    input  wire        [width_dds - 1:0]    K,              // phase constant for DDS
+    output wire signed [15:0]               demodulated);   // demodulated signal
 
-   wire                                 adc_s;            // synchronized broadcast signal from 1-bit ADC
-   bit  signed [width_dds - 1:0]        phase;            // DDS phase
-   wire signed [1:0]                    I, Q;             // I/Q
-   wire signed [2 + $clog2(M**3) - 1:0] If, Qf;           // filtered I/Q
-   wire signed [width_cordic - 1:0]     cordic_phase;     // CORDIC phase
+   wire                                 adc_s;              // synchronized broadcast signal from 1-bit ADC
+   bit  signed [width_dds - 1:0]        phase;              // DDS phase
+   wire signed [1:0]                    I, Q;               // I/Q
+   wire signed [2 + $clog2(M1**3) - 1:0] If, Qf;            // filtered I/Q
+   wire signed [width_cordic - 1:0]     cordic_phase;       // CORDIC phase
+   wire signed [width_cordic - 1:0]     differentiator_out; // differentiator output
+   wire signed [width_cordic + $clog2(M2**3) - 1:0] demodulated_f; // filtered demodulated
 
    dds
      #(.width(width_dds))
@@ -39,7 +43,7 @@ module radio_core
       .Q);
 
    cic_3_filter
-     #(.M    (M),
+     #(.M    (M1),
        .width(2))
    filter_I
      (.reset,
@@ -49,7 +53,7 @@ module radio_core
       .out    (If));
 
    cic_3_filter
-     #(.M    (M),
+     #(.M    (M1),
        .width(2))
    filter_Q
      (.reset,
@@ -77,6 +81,17 @@ module radio_core
      (.reset,
       .clk(clk_b),
       .in(cordic_phase),
-      .out(demodulated));
+      .out(differentiator_out));
 
+   cic_3_filter
+     #(.M    (M2),
+       .width(width_cordic))
+   filter_audio
+     (.reset,
+      .clk_in (clk_b),
+      .clk_out(clk_a),
+      .in     (differentiator_out),
+      .out    (demodulated_f));
+
+   assign demodulated = demodulated_f[$left(demodulated_f) : 16];
 endmodule
