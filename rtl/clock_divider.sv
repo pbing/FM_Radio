32831f89776dead9f7c57ce9712ac3927fab1c1e
Bernd Beuster <bernd.beuster@creativechips.com>
2016-06-02 16:59:46 +0200

    Initial commit.

---
+++ rtl/clock_divider.sv
@@ -0,0 +1,30 @@
+/* Generic clock divider */
+
+module clock_divider
+  #(parameter M)        // divider ratio
+   (input  wire  reset,  // reset
+    input  wire  clk,    // input clock
+    output logic clk_o); // output clock
+
+   logic [$clog2(M) - 1 : 0] counter;
+
+   always_ff @(posedge clk_s or posedge reset)
+     if (reset)
+       begin
+          counter <= '0;
+          clk_o   <= 1'b0;
+       end
+     else
+       if (counter == M/2 - 1)
+         begin
+            counter <= counter + 1;
+            clk_o   <= 1'b0;
+         end
+       else if (counter == M - 1)
+         begin
+            counter <= '0;
+            clk_o   <= 1'b1;
+         end
+       else
+         counter <= counter + 1;
+endmodule
