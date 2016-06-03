54b62fb75b9455c6b6a629a410b42de6f47b8fb6
Bernd Beuster <bernd.beuster@creativechips.com>
2016-06-03 10:10:37 +0200

    Changed CIC to FPGA clocking.

--- rtl/cic_3_filter.sv
+++ rtl/cic_3_filter.sv
@@ -1,18 +1,19 @@
 /* Pipelined decimating Cascaded Integrator-Comb (CIC) filter of order 3. */
 
 module cic_3_filter
-  #(parameter M,
-    parameter width)
+  #(parameter R,                                               // decimation ratio
+    parameter width)                                           // input data width
    (input  wire                                       reset,   // reset
-    input  wire                                       clk_in,  // clock
-    input  wire                                       clk_out, // decimated clock
+    input  wire                                       clk,     // clock
+    input  wire                                       en_in,   // clock enable input
+    input  wire                                       en_out,  // clock enable output
     input  wire signed [width - 1                : 0] in,      // input
-    output wire signed [width + $clog2(M**3) - 1 : 0] out);    // filtered and decimated output
+    output wire signed [width + $clog2(R**3) - 1 : 0] out);    // filtered and decimated output
 
-   logic signed [width + $clog2(M**3) - 1 : 0] i_del[3], d_del[3], d_dif[3];
+   logic signed [width + $clog2(R**3) - 1 : 0] i_del[3], d_del[3], d_dif[3];
 
    /* integrator section */
-   always_ff @(posedge clk_in or posedge reset)
+   always_ff @(posedge clk or posedge reset)
      if (reset)
        begin
           i_del[0] <= '0;
@@ -20,14 +21,15 @@ module cic_3_filter
           i_del[2] <= '0;
        end
      else
-       begin
-          i_del[0] <= in       + i_del[0];
-          i_del[1] <= i_del[0] + i_del[1];
-          i_del[2] <= i_del[1] + i_del[2];
-       end
+       if (en_in)
+         begin
+            i_del[0] <= in       + i_del[0];
+            i_del[1] <= i_del[0] + i_del[1];
+            i_del[2] <= i_del[1] + i_del[2];
+         end
 
    /* differentiator section */
-   always_ff @(posedge clk_out or posedge reset)
+   always_ff @(posedge clk or posedge reset)
      if (reset)
        begin
           d_dif[0] <= '0;
@@ -39,15 +41,16 @@ module cic_3_filter
           d_del[2] <= '0;
        end
      else
-       begin
-          d_dif[0] <= i_del[2] - d_del[0];
-          d_dif[1] <= d_dif[0] - d_del[1];
-          d_dif[2] <= d_dif[1] - d_del[2];
+       if (en_out)
+         begin
+            d_dif[0] <= i_del[2] - d_del[0];
+            d_dif[1] <= d_dif[0] - d_del[1];
+            d_dif[2] <= d_dif[1] - d_del[2];
 
-          d_del[0] <= i_del[2];
-          d_del[1] <= d_dif[0];
-          d_del[2] <= d_dif[1];
-       end
+            d_del[0] <= i_del[2];
+            d_del[1] <= d_dif[0];
+            d_del[2] <= d_dif[1];
+         end
 
    assign out = d_dif[2];
 endmodule
--- sim/cic_3_filter/tb_cic_3_filter.sv
+++ sim/cic_3_filter/tb_cic_3_filter.sv
@@ -2,54 +2,59 @@ module tb_cic_3_filter;
    timeunit 1ns;
    timeprecision 1ps;
 
-   localparam M     = 10;
-   localparam width = 11;
+   localparam R     = 5; // decimation ratio
+   localparam width = 2; // input data width
 
-   const realtime tclk_in = 1s/250e6;
+   const realtime tclk = 1s/250e6;
 
-   bit                                        reset;
-   bit                                        clk_in;
-   bit                                        clk_out;
-   bit  signed [width - 1 : 0]                in;
-   wire signed [width + $clog2(M**3) - 1 : 0] out;
+   bit                                        reset;         // reset
+   bit                                        clk;           // clock
+   const bit                                  en_in = 1'b1;  // clock enable input
+   bit                                        en_out;        // clock enable output
+   bit  signed [width - 1 : 0]                in;            // input
+   wire signed [width + $clog2(R**3) - 1 : 0] out;           // filtered and decimated outpu
 
    cic_3_filter
-     #(.M    (M),
+     #(.R    (R),
        .width(width))
    dut(.*);
 
-   always #(tclk_in/2)  clk_in  = ~clk_in;
+   always #(tclk/2)  clk  = ~clk;
 
-   always @(posedge clk_in)
+   always @(posedge clk)
      begin:clk_gen
         int counter;
 
-        if (counter < M/2)
-          clk_out <= 1'b1;
+        if (counter == R - 1)
+          begin
+             counter <= 0;
+             en_out  <= 1'b1;
+          end
         else
-          clk_out <= 1'b0;
-
-        if (counter < M - 1)
-          counter <= counter + 1;
-        else
-          counter <= 0;
+          begin
+             counter <= counter + 1;
+             en_out  <= 1'b0;
+          end
      end:clk_gen
 
    initial
      begin:main
         reset = 1'b1;
-        @(negedge clk_out);
+        @(negedge clk);
         reset = 1'b0;
 
-        @(posedge clk_out);
-        in <= 'sd1000;
-        repeat (2 * 3 * M) @(posedge clk_out);
+        @(posedge clk);
+        in <= 1;
+        repeat (2 * 3 * R) @(posedge clk);
+        assert (out == R**3 * in);
 
-        in <= -'sd1000;
-        repeat (2 * 3 * M) @(posedge clk_out);
+        in <= -1;
+        repeat (2 * 3 * R) @(posedge clk);
+        assert (out == R**3 * in);
 
-        in <= 'sd0;
-        repeat (2 * 3 * M) @(posedge clk_out);
+        in <= 0;
+        repeat (2 * 3 * R) @(posedge clk);
+        assert (out == R**3 * in);
 
         $finish;
      end:main
